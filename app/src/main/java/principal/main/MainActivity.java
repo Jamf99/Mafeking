c1ccf1d0645f79371d3c809eb9093869652feda3
Jorge Antonio Morales Florez <jamf_99@hotmail.com>
2019-05-16 12:55:12 -0500

    Vista principal de la aplicación completada

--- app/src/main/java/principal/main/MainActivity.java
+++ app/src/main/java/principal/main/MainActivity.java
@@ -1,12 +1,9 @@
 package principal.main;
 
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.Window;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
@@ -18,9 +15,6 @@ public class MainActivity extends AppCompatActivity {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.principal);
         mTextMessage = findViewById(R.id.message);
-
-        Toast.makeText(this, "Hello, i'm here", Toast.LENGTH_LONG).show();
-
     }
 
 }
